36eb67d1a9a55f3cd20df6ef3bc17e35c97f901b
ohddang <ohddang509@gmail.com>
2024-01-06 18:21:59 +0900

    setting

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-xl font-bold underline text-blue-600">
+        Hello world!
+      </h1>
+      <p className="text-4xl text-blue-600/100">The quick brown fox...</p>
+      <p className="text-blue-600/75">The quick brown fox...</p>
+      <p className="text-blue-600/50">The quick brown fox...</p>
+      <p className="text-blue-600/25">The quick brown fox...</p>
+      <p className="text-blue-600/0">The quick brown fox...</p>
+    </div>
+  );
+}
+
+export default App;
