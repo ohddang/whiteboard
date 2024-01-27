9816b7134a291b6795e2f582eb9d2178a5236adb
ohddang <ohddang509@gmail.com>
2024-01-27 14:26:46 +0900

    CRA -> vite 환경 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
+  base: "/whiteboard/",
   build: {
     outDir: "build",
   },
