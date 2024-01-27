2e70178f56b41589421e1aafb07caa139ad5ca7c
ohddang <ohddang509@gmail.com>
2024-01-27 14:51:56 +0900

    deploy: vite change build dir path ( build -> dist )

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 export default defineConfig({
   base: "/whiteboard/",
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   plugins: [reactRefresh()],
   esbuild: {
